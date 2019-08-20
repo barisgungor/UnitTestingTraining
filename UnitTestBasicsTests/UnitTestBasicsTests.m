2b727dbbf84d4b4cb0cad5db9735090883141041
Barış Güngör <gungor.baris@outlook.com>
2019-08-20 14:36:17 +0300

    test

---
+++ UnitTestBasicsTests/UnitTestBasicsTests.m
@@ -0,0 +1,37 @@
+//
+//  UnitTestBasicsTests.m
+//  UnitTestBasicsTests
+//
+//  Created by Barış Güngör on 20.08.2019.
+//  Copyright © 2019 Barış Güngör. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UnitTestBasicsTests : XCTestCase
+
+@end
+
+@implementation UnitTestBasicsTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
